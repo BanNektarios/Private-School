41aaefd42490de416b1618e1f979554501634952
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-01-30 22:37:41 +0200

    added routes for all gets/views + landing page

--- routes/list.js
+++ routes/list.js
@@ -2,37 +2,66 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET Students page. */
-router.get('/', function (req, res, next) {
+/* GET Lists page. */
+router.get('/',  (req, res, next) => {
     res.render('list', {title: 'Lists', headings: 'Choose what list you want to see!'})
 });
 
 /* GET Students page. */
-router.get('/students', function (req, res, next) {
+router.get('/students',  (req, res, next) => {
     var dbConnection = require('../lib/db');
     const query = "SELECT * FROM students"
     dbConnection.query(query, (err, rows) => {
         if (err) {
             res.render('list', { title: "List - ERROR" })
         } else {
-            res.render('students', { title: 'Students', students: rows });
+            res.render('students', { title: 'Students', headings: 'A list of students', students: rows });
         }
     })
 });
 
 
 /* GET Courses page. */
-router.get('/courses/:message?', function (req, res, next) {
+router.get('/courses/:message?',  (req, res, next) => {
     var dbConnection = require('../lib/db');
     const query = "SELECT * FROM courses"
     dbConnection.query(query, (err, rows) => {
         if (err) {
             res.render('list', { title: "List - ERROR" , headings: 'Got And error on "/courses"'})
         } else {
-            res.render('courses', { title: 'Courses', courses: rows });
+            res.render('courses', { title: 'Courses', headings: 'A list of courses', courses: rows });
         }
     })
 });
+
+/* GET Trainers page. */
+router.get('/trainers/:message?',  (req, res, next) => {
+    var dbConnection = require('../lib/db');
+    const query = "SELECT * FROM trainers"
+    dbConnection.query(query, (err, rows) => {
+        if (err) {
+            res.render('list', { title: "List - ERROR" , headings: 'Got And error on "/trainers"'})
+        } else {
+            res.render('trainers', { title: 'Trainers', headings: 'A list of trainers', trainers: rows });
+        }
+    })
+});
+
+/* GET Trainers page. */
+router.get('/assignments/:message?',  (req, res, next) => {
+    var dbConnection = require('../lib/db');
+    const query = "SELECT * FROM assignments"
+    dbConnection.query(query, (err, rows) => {
+        if (err) {
+            res.render('list', { title: "List - ERROR" , headings: 'Got And error on "/assignments"'})
+        } else {
+            res.render('assignments', { title: 'Assignments', headings: 'A list of assignments', assignments: rows });
+        }
+    })
+});
+
+router.post('trainers', )
+
 
 
 
